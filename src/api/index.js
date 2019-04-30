4853d7435840492914ce9a9c213b9dbc8f4304eb
张震 <1191@mapgoo.net>
2019-04-30 18:22:12 +0800

    1.代码提交~~~~~~~~~~~~~~~~~~

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 //const apiUrl = 'http://test';//测试域名，自己改成自己的
- const apiUrl = 'http://192.168.110.19:8888';//线上域名，自己改成自己的 
+ // const apiUrl = 'http://192.168.110.19:8888';//线上域名，自己改成自己的 
+ const apiUrl = 'https://api.ublog.top';//线上域名，自己改成自己的 
 
  export default apiUrl
